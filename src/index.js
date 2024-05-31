9097aa0a0cb4de95246823191b8e4e5573422498
deepak0019 <dangideepak0018@gmail.com>
2024-06-01 01:56:21 +0530

    added code for file handling, setup done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+import  express  from "express";
+
 dotenv.config({
   path: "./env",
 });
+
+const app = express();
 
 connectDB()
   .then(() => {
