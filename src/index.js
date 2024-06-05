0fdb34ef1eb9d7d842064a45077b06bea2689866
deepak0019 <dangideepak0018@gmail.com>
2024-06-06 00:23:17 +0530

    Add registerController and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-import  express  from "express";
+import express from "express";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-const app = express();
-
 connectDB()
   .then(() => {
     const port = process.env.PORT || 8000;
 
-    app.on("error", (error) => {
-      console.log("ERR: ", error);
-      throw error;
-    });
+    // app.on("error", (error) => {
+    //   console.log("ERR: ", error);
+    //   throw error;
+    // });
 
     app.listen(port, () => {
       console.log(`Server is running at PORT: ${port}`);
