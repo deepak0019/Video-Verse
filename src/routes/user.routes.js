a47550965a3c4712d5a5c24886539d5c3995525e
deepak0019 <dangideepak0018@gmail.com>
2024-06-06 16:41:27 +0530

    Added Refreshtoken and accessToken logic

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,6 +24,7 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
+//secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
